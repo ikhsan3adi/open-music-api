c1f76a580f9304a92006f1b5ba7dc0d577e8798f
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-06-16 17:32:14 +0700

    create additional migration

---
+++ migrations/1686881104937_create-table-playlists.js
@@ -0,0 +1,20 @@
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlists');
+};
---
+++ migrations/1686882868783_add-foreign-key-to-owner-column.js
@@ -0,0 +1,7 @@
+exports.up = (pgm) => {
+  pgm.addConstraint('playlists', 'fk_playlists.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlists', 'fk_playlists.owner_users.id');
+};
